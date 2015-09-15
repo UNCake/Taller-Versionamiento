2ba25736a2097a80c7545039720ba412c67de812
anmrdz <anmrdz@github.com>
2015-09-15 15:38:16 -0500

    Added setters and getters to Post class

--- Post.java
+++ Post.java
@@ -9,6 +9,38 @@ class Post {
     private Date dateCreated;
     private Date lastupdate;
     private boolean isAllowed;
+    
+    public String getTopic() {
+        return topic;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Date getLastupdate() {
+        return lastupdate;
+    }
+
+    public void setLastupdate(Date lastupdate) {
+        this.lastupdate = lastupdate;
+    }
+
+    public boolean isIsAllowed() {
+        return isAllowed;
+    }
+
+    public void setIsAllowed(boolean isAllowed) {
+        this.isAllowed = isAllowed;
+    }
 
     public static void main(String[] args) throws
             java.lang.Exception {
