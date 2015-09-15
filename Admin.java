8e5b0f6bae5db4947a11f5f781c0541a628b4a29
santicoxd <scarvajalc@unal.edu.co>
2015-09-15 15:37:51 -0500

    Seter y Geter agregados

--- Admin.java
+++ Admin.java
@@ -1,4 +1,20 @@
 public class Admin {
     private int level;
     private double rating;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
 }
